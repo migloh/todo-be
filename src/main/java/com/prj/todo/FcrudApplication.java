3d56931f4db1423c39b47d93f2a1f26187a8832f
Milos PHAM <minhlong.wb@gmail.com>
2025-04-25 02:01:07 +0700

    First commit

---
+++ src/main/java/com/prj/todo/FcrudApplication.java
@@ -0,0 +1,13 @@
+package com.prj.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prj/todo/FcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prj.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
